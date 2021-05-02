a10b20327b3e2e2fa08a568f803a811d9134798c
Austin Howard <cnfishead@hotmail.com>
2021-05-02 18:08:11 -0400

    Added new components, and dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./Screens/HomeScreen";
 import { Container } from "react-bootstrap";
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <Container>
         <main className="py-3">
-          <h1>Welcome to the eCommerce page</h1>
+          <Home />
         </main>
       </Container>
       <Footer />
