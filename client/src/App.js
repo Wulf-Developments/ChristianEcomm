716bfc3a5cfcc3ed2f3a795e7ed6a897dbe88568
Austin Howard <cnfishead@hotmail.com>
2021-05-02 17:42:00 -0400

    Init setup of Webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome to the eCommerce page</h1>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Welcome to the eCommerce page</h1>
+        </main>
+      </Container>
+      <Footer />
     </div>
   );
 };
