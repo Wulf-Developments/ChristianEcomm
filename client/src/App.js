1361020b4d7a8bdee38cec447b6cc1afd924cca4
Austin Howard <cnfishead@hotmail.com>
2021-05-02 18:57:31 -0400

    added in product pages, you can now view individual products in more depth

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Container>
-        <main className="py-3">
-          <Home />
-        </main>
-      </Container>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Container>
+          <main className="py-3">
+            <Route path="/" component={Home} exact />
+            <Route path="/product/:id" component={ProductScreen} />
+          </main>
+        </Container>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
