03e416b9735aca4715a0aa87e5c4850959dea319
Austin Howard <cnfishead@hotmail.com>
2021-05-03 11:29:36 -0400

    added in redux, removed useState, created actions/constants/reducers

---
+++ client/src/store.js
@@ -0,0 +1,28 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+
+//create reducer
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
+
+//init state
+const initialState = {};
+
+//init middleware
+const middleware = [thunk];
+
+//create store
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
