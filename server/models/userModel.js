560ca834a201d4ea30ba0f51d895986c22713e70
Austin Howard <cnfishead@hotmail.com>
2021-05-03 15:09:49 -0400

    finished user registration

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -30,6 +30,16 @@ const userSchema = mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcyrpt.compare(enteredPassword, this.password);
 };
+
+// Should hash the password on registration.
+userSchema.pre("save", async function (next) {
+  //conditional will check to see if the password is being modified so it wont update the password constantly.
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcyrpt.genSalt(10);
+  this.password = await bcyrpt.hash(this.password, salt);
+});
 const User = mongoose.model("User", userSchema);
 
 export default User;
