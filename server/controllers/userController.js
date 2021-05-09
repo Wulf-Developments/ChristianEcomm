70c12bf420ed376c7b644de2c622a1dda82b7e56
Austin Howard <cnfishead@hotmail.com>
2021-05-09 18:10:08 -0400

    fixed admin middleware, added route to fetch all User Profiles

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -103,4 +103,13 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+//@desc get All User profiles.
+//@route Get /api/users
+//@access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  //Finds ALL users
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
