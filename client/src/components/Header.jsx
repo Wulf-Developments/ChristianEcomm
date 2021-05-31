15b61e45214329df7d6f2716e5170400464e2127
Austin Howard <cnfishead@hotmail.com>
2021-05-30 20:12:56 -0400

    showcase

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
       <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>eCommerce Page</Navbar.Brand>
+            <Navbar.Brand>Jared's 3D Models</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
